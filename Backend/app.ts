9e7674d671937fc48e71c0274d5690e4f42a3285
godspeed-03 <satyam2001anand@gmail.com>
2024-09-15 05:59:07 +0530

    fix : fixed cors error env

--- Backend/app.ts
+++ Backend/app.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // or your frontend origin
+  origin: `${process.env.CLIENT_URL}`, 
   credentials: true,
 }));
 app.use(cookieParser());
